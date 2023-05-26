9ca5be5c3ac7e7d1df62f5ea3c5509c353d47ba4
Patrick <chodowski.patrick@gmail.com>
2023-05-26 10:13:11 +0400

    redoing dt

--- src/decision_tree.rs
+++ src/decision_tree.rs
@@ -1,53 +1,67 @@
 
-use core::cmp::Ordering::Equal;
-use crate::machine_learning::MachineLearning;
+// use core::cmp::Ordering::Equal;
+use crate::machine_learning::ML;
 
-fn split_data(split_variable: String, split_value: f32) {
-}
+// fn split_data(split_variable: String, split_value: f32) {
+// }
 
-pub fn check_if_boolean(unique_values: &mut Vec<f32>) -> bool {
-  return unique_values.len() <= 2;
+// pub fn check_if_boolean(unique_values: &mut Vec<f32>) -> bool {
+//   return unique_values.len() <= 2;
+// }
+
+pub enum DTCritetion {
+  Gini,
+  InformationGain
 }
 
 
 
 pub struct DecisionTree<'a> {
-  pub ml: MachineLearning<'a>,
-  pub criterion: String,
+  pub ml: &'a ML<'a>,
+  pub criterion: DTCritetion,
 }
 
-impl <'a> DecisionTree<'a>{
-  pub fn info(&self){
-    println!("Decision Tree. Criterion: {}; Index: {}; Target: {}", &self.criterion, &self.ml.index, &self.ml.target);
+impl<'a> DecisionTree <'_> {
+  pub fn init(ml: &'a ML, criterion: DTCritetion) -> DecisionTree<'a> {
+    return DecisionTree {ml, criterion};
   }
 
-  pub fn gini_impurity(&self){
-    // Gini = 1 - sum(pi)^2
-    todo!()
+  pub fn train(self) {
+    println!("brrrr... training");
   }
-
-  pub fn information_gain(&self){
-    // Gini = 1 - sum(pi)^2
-    todo!()
-  }
-
-  pub fn get_unique_values(&mut self, variable_name: &'a str) -> &mut Vec<f32> {
-
-    println!("Getting unique values for {}", variable_name);
-    let base_v = self.ml.data.entry(variable_name).or_default(); 
-    base_v.sort_by(|a, b| a.partial_cmp(b).unwrap_or(Equal));
-    base_v.dedup();
-
-    // let iter_v = base_v.into_iter();
-    // for a in iter_v {
-    //   println!("{}", a)
-    // }
-    println!("Found {} unique values for {}", base_v.len(), variable_name);
-    return base_v;
-  }
-
-
-
-
 
 }
+
+
+
+
+// impl DecisionTree{
+//   pub fn info(&self){
+//     println!("Decision Tree. Criterion: {}; Index: {}; Target: {}", &self.criterion, &self.ml.index, &self.ml.target);
+//   }
+
+//   pub fn gini_impurity(&self){
+//     // Gini = 1 - sum(pi)^2
+//     todo!()
+//   }
+
+//   pub fn information_gain(&self){
+//     // Gini = 1 - sum(pi)^2
+//     todo!()
+//   }
+
+//   pub fn get_unique_values(&mut self, variable_name: &'a str) -> &mut Vec<f32> {
+
+//     println!("Getting unique values for {}", variable_name);
+//     let base_v = self.ml.data.entry(variable_name).or_default(); 
+//     base_v.sort_by(|a, b| a.partial_cmp(b).unwrap_or(Equal));
+//     base_v.dedup();
+
+//     // let iter_v = base_v.into_iter();
+//     // for a in iter_v {
+//     //   println!("{}", a)
+//     // }
+//     println!("Found {} unique values for {}", base_v.len(), variable_name);
+//     return base_v;
+//   }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod machine_learning;
-// mod decision_tree;
+mod decision_tree;
 mod utils;
 
-// use decision_tree::{DecisionTree, check_if_boolean};
+use decision_tree::{DecisionTree, DTCritetion};
 use machine_learning::{ML, MLType};
 use utils::{read_csv, preprocess};
 
@@ -12,6 +12,9 @@ fn main() {
   let mut data = read_csv("./data/train.csv").ok().unwrap();
   data = preprocess(data);
   let ml = ML::new(&mut data, "Survived", "PassengerId", MLType::Classification);
+  let dt = DecisionTree::init(&ml, DTCritetion::Gini);
+  dt.train();
+
   // println!("{}", ml.df);
   // println!("{}", ml.index);
   // println!("{}", ml.target);
