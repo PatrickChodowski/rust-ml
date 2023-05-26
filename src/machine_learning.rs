0cfdd20216ae8237cf8d5b7f21c3b1ba4a0fb683
Patrick <chodowski.patrick@gmail.com>
2023-05-26 09:49:32 +0400

    cleaning ml object

--- src/machine_learning.rs
+++ src/machine_learning.rs
@@ -1,74 +1,33 @@
 
+use polars::prelude::*;
 
-// Contains base trait for machine learning struct
-
-use crate::utils::DataFrame;
-
-pub struct MachineLearning<'a> {
-  pub data: DataFrame<'a>, // Training data read from CSV
-  pub target: String, // Name of target attribute
-  pub index: String // Name of index attribute
+pub enum MLType {
+  Classification,
+  Regression
 }
 
-impl <'a> MachineLearning<'a>{
 
-  pub fn query_larger(&mut self, 
-                where_column: &'a str, 
-                where_value: &mut f32)  -> Vec<bool> {
+pub struct ML <'a> {
+  pub df: &'a mut DataFrame,
+  pub target_name: String, // Name of target attribute
+  pub index_name: String,   // Name of index attribute
+  pub target: Series,
+  pub index: Series,
+  pub ml_type: MLType
+}
 
-    let mut res: Vec<bool> = Vec::new();
-    let base_v = self.data.entry(where_column).or_default(); 
-    let iter_v = base_v.into_iter();
 
-    for a in iter_v {
-      if a >= where_value {
-        println!("{}", a);
-        res.push(true);
-      } else {
-        res.push(false);
-      }
+impl<'a> ML <'a> {
+    pub fn new(df: &'a mut DataFrame, target: &str, index: &str, ml_type: MLType) -> ML<'a> {
+
+      let ml = ML{
+        target_name: target.to_string(), 
+        index_name: index.to_string(), 
+        target: df.drop_in_place(target).ok().unwrap(),
+        index: df.drop_in_place(index).ok().unwrap(),
+        ml_type,
+        df
+      };
+      return ml;
     }
-
-    return res;
-  }
-
-  pub fn query_smaller(&mut self, 
-                       where_column: &'a str, 
-                       where_value: &mut f32) -> Vec<bool> {
-    let mut res: Vec<bool> = Vec::new();
-    let base_v = self.data.entry(where_column).or_default(); 
-    let iter_v = base_v.into_iter();
-
-    for a in iter_v{
-      if a < where_value {
-        println!("{}", a);
-        res.push(true);
-      } else {
-        res.push(false);
-      }
-    }
-
-    return res;
-  }
-
-  pub fn count_targets(&'a mut self, query_v: &Vec<bool>) -> f32 {
-    let mut total_targets: f32 = 0.0;
-    let mut total_obs: f32 = 0.0;
-
-    let targets_v = &self.data.entry(&self.target).or_default();
-    let iter_query_v = query_v.iter();
-
-
-    for (index, a) in iter_query_v.enumerate(){
-      if *a {
-        total_targets += targets_v[index];
-        total_obs += 1.0;
-      }
-    }
-    println!("Sum target positive: {} out of {}", total_targets, total_obs);
-
-    return total_targets;
-  }
-
-
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-// mod machine_learning;
+mod machine_learning;
 // mod decision_tree;
 mod utils;
 
 // use decision_tree::{DecisionTree, check_if_boolean};
-// use machine_learning::MachineLearning;
+use machine_learning::{ML, MLType};
 use utils::{read_csv, preprocess};
 
 
@@ -11,29 +11,11 @@ fn main() {
   // if there is no data it might as well fail immediately
   let mut data = read_csv("./data/train.csv").ok().unwrap();
   data = preprocess(data);
-
-  println!("data: {:?}", data);
-  println!("columns: {:?}", data.get_column_names());
-
-
-
+  let ml = ML::new(&mut data, "Survived", "PassengerId", MLType::Classification);
+  // println!("{}", ml.df);
+  // println!("{}", ml.index);
+  // println!("{}", ml.target);
 
 
-  // let mut dt: DecisionTree = DecisionTree{
-  //   ml: MachineLearning{data: data, 
-  //                       target: "survived".into(), 
-  //                       index: "passenger_id".into()},
-  //   criterion: "gini".into()
-  // };
-
-  // dt.info();
-  // let fare_values: &mut Vec<f32> = dt.get_unique_values("fare".into());
-  // check_if_boolean(fare_values);
-
-  // let v_larger = dt.ml.query_larger("fare", &mut 0.0);
-  // let v_smaller = dt.ml.query_smaller("fare", &mut 0.0);
-
-  // dt.ml.count_targets(&v_larger);
-  // // dt.ml.count_tarsgets(&v_smaller);
 
 }
